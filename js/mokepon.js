0fc77143ef4c045d6cb229e8e226f3817fa35c41
abimaelbaez <abimael.baez@gmail.com>
2022-08-29 16:14:21 -0500

    Obstaculos y colisiones

--- js/mokepon.js
+++ js/mokepon.js
@@ -407,6 +407,14 @@ function pintarCanvas() {
     viudanegraEnemigo.pintarMokepon()
     antmanEnemigo.pintarMokepon()
     chinchillaEnemigo.pintarMokepon()
+    if(mascotaJugadorObjeto.velocidadX !== 0 || mascotaJugadorObjeto.velocidadY !== 0) {
+        revisarColision(hipodogeEnemigo)
+        revisarColision(capipepoEnemigo)
+        revisarColision(ratigueyaEnemigo)
+        revisarColision(viudanegraEnemigo)
+        revisarColision(antmanEnemigo)
+        revisarColision(chinchillaEnemigo)
+    }
 }
 
 
@@ -469,6 +477,34 @@ function obtenerObjetoMascota(){
         }
         
     }
+}
+
+function revisarColision(enemigo) {
+    const arribaEnemigo = enemigo.y
+    const abajoEnemigo = enemigo.y + enemigo.alto
+    const derechaEnemigo = enemigo.x + enemigo.ancho
+    const izquierdaEnemigo = enemigo.x
+
+    const arribaMascota =
+     mascotaJugadorObjeto.y
+    const abajoMascota = 
+    mascotaJugadorObjeto.y + mascotaJugadorObjeto.alto
+    const derechaMascota = 
+    mascotaJugadorObjeto.x + mascotaJugadorObjeto.ancho
+    const izquierdaMascota = 
+    mascotaJugadorObjeto.x
+
+    if(
+        abajoMascota < arribaEnemigo ||
+        arribaMascota > abajoEnemigo ||
+        derechaMascota < izquierdaEnemigo ||
+        izquierdaMascota > derechaEnemigo
+    ) {
+        return
+    }
+
+    detenerMovimiento()
+    alert("hay colision" + enemigo.nombre)
 }
 
 window.addEventListener('load', iniciarJuego)
